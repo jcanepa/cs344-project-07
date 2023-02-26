cfd74dc984d7cae6d555576a1cd4eb2f27a7ea2c
Julian Canepa <julian.l.canepa@gmail.com>
2023-02-25 21:55:18 -0500

    Update comments around new block placement.

--- myalloc.c
+++ myalloc.c
@@ -47,8 +47,8 @@ void *myalloc(int size)
 
                 // "wire" new block node into the linked list
                 int b_to_new_offset = (padded_block_size + padded_requested_space);
-                b[b_to_new_offset] = new;        // placing it in memory at a given location
-                b->next = &(b[b_to_new_offset]); // setting it as the current block's next pointer
+                b[b_to_new_offset] = new;        // place in memory at a relative location
+                b->next = &(b[b_to_new_offset]); // set new block as current block's next pointer
             }
 
             b->in_use = 1;
